90cd52b538810dc783974d690ac7e0cc50cd7c30
korigamik <korigamik@gmail.com>
2022-12-17 21:12:22 +0530

    print topology

--- create-training-data.py
+++ create-training-data.py
@@ -1,5 +1,13 @@
 import random
 
+"""
+This script creates a simple XOR prediction training data set for the neural network.
+save the output set to a file called training-data.txt in the test build directory.
+
+run: `python create-training-data.py > training-data.txt`
+"""
+
+print("topology: 2 4 1")
 for i in range(2000):
     n1 = random.randint(0, 1)
     n2 = random.randint(0, 1)
